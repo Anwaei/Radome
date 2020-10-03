144dc102e1eb1d7598a573af06eee57d38b63e4d
Anwaei <civact@qq.com>
2020-10-03 13:44:26 +0800

    1

--- errSlxParas.m
+++ errSlxParas.m
@@ -1,10 +1,10 @@
 clear
 syms s
 n = 5; N = 4; Ta_Tg = 3; Vc_Vm = 1.5; Tf_Tg = 15;
-R = 0.03;
+R = -0.03;
 eq_num = N*s/(1-R);
 eq_den = (1/n*s+1)^n + N*Vc_Vm*R/(1-R)*(Ta_Tg*s+1);
-coeffs_num = double(fliplr(coeffs(eq_num,s)));
+coeffs_num = [double(fliplr(coeffs(eq_num,s))) 0];
 coeffs_den = double(fliplr(coeffs(eq_den,s)));
 
 Tg = 1; Vm = 1; aT = 1; epi = 1; Tf = 1; phiGN = 1;
